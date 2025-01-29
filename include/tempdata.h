cdc5413b8433e720a5d4ab9d95f5a46fdb1cd355
xXHighTideXx <84487860+TheHighTide@users.noreply.github.com>
2025-01-29 11:04:15 -0800

    Create tempdata.h
    Just made a tempdata file

---
+++ include/tempdata.h
@@ -0,0 +1,10 @@
+#ifndef TEMPLATE_DATA_H
+#define TEMPLATE_DATA_H
+
+#include <iostream>
+
+void GetTemplateData() {
+	std::cout << "This function hasn't been fully completed yet..." << std::endl;
+}
+
+#endif
